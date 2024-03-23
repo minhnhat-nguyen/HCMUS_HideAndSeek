338745088d90e7efc3aaaa5e77606d441d560330
minhnhat <minhnhatnguyen.aug@gmail.com>
2024-03-23 10:23:48 +0700

    it works somehow, something is so wrong with the access modifiers

--- Agent.py
+++ Agent.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import pygame
+import random
 
 class position(ABC):
     def __init__(self, x : int, y: int):
@@ -9,9 +10,9 @@ class position(ABC):
 class Agent:
     def __init__(self, x: int, y: int) -> None:
         self.position = position(x, y)
-    
+        self.point = 100
     @abstractmethod
-    def move(self):
+    def move(self, moves : list[position]) -> position:
         pass
 
 
@@ -19,18 +20,22 @@ class Hider(Agent):
     def __init__(self, x: int, y: int) -> None:
         super().__init__(x, y)
         self.isFound = False
-    def move(self):
-        pass
+    def move(self, moves : list[position]) -> position:
+        self.position = random.choice(moves)
+        return self.position
 
 class Seeker(Agent):
     def __init__(self, x: int, y: int) -> None:
         super().__init__(x, y)
-        self.__hiderLastPos = position(0, 0)
-    def move(self):
-        pass
+        self.hiderLastPos = position(0, 0)
+    def move(self, moves : list[position]) -> position:
+        self.position = random.choice(moves)
+        return self.position
 
 class GameMaster:
     def __init__(self, filename: str) -> None:
+        self._hider = Hider(0, 0)
+        self._seeker = Seeker(0, 0)
         with open(filename, 'r') as file:
             self._n, self._m = [int(x) for x in next(file).split()]
             self._map = [[int(x) for x in line[:-1]] for line in file]
@@ -40,7 +45,26 @@ class GameMaster:
                         self._hider = Hider(i, j)
                     elif self._map[i][j] == 3:
                         self._seeker = Seeker(i, j)
-    def update_screen(self, screen: pygame.Surface):
+
+    def is_game_over(self) -> bool:
+        return self._hider.isFound
+
+    def _get_posible_moves(self, ag: Agent) -> list[position]:
+        moves : list[position] = []
+        x = ag.position.x
+        y = ag.position.y
+        if x - 1 >= 0 and self._map[x - 1][y] != 1:
+            moves.append(position(x - 1, y))
+        if x + 1 < self._n and self._map[x + 1][y] != 1:
+            moves.append(position(x + 1, y))
+        if y - 1 >= 0 and self._map[x][y - 1] != 1:
+            moves.append(position(x, y - 1))
+        if y + 1 < self._m and self._map[x][y + 1] != 1:
+            moves.append((position(x, y + 1)))
+        return moves
+
+    def _update_screen(self, screen: pygame.Surface):
+        screen.fill((255, 255, 255))
         for i in range(self._n):
             for j in range(self._m):
                 if self._map[i][j] == 1:
@@ -59,9 +83,17 @@ class GameMaster:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     return
-            screen.fill((255, 255, 255))
-            self.update_screen(screen)
-            pygame.time.wait(1000)
+            while True:
+                self._update_screen(screen)
+                if self.is_game_over():
+                    break
+                previousSeekerPos = self._seeker.position
+                self._seeker.move(self._get_posible_moves(self._seeker))
+                self._map[previousSeekerPos.x][previousSeekerPos.y] = 0
+                self._map[self._seeker.position.x][self._seeker.position.y] = 3
+                if self._seeker.position.x == self._hider.position.x and self._seeker.position.y == self._hider.position.y:
+                    self._hider.isFound = True
+                pygame.time.delay(500)
 
 if __name__ == '__main__':
     game = GameMaster('map.txt')
