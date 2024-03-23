da55013a225fb4c36dc2532e6fd28c9c9adfcc2c
Minh Nhat <minhnhatnguyen.aug@gmail.com>
2024-03-23 16:34:03 +0700

    small refactor

--- Agent.py
+++ Agent.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import pygame
 import random
+import heapq
+import copy
 
 class position(ABC):
     def __init__(self, x : int, y: int):
@@ -12,30 +14,50 @@ class Agent:
         self.position = position(x, y)
         self.point = 100
     @abstractmethod
-    def move(self, moves : list[position]) -> position:
+    def move(self, map : list[list[int]]) -> position:
         pass
+
+    def _get_posible_moves(self, map : list[list[int]]) -> list[position]:
+        moves : list[position] = []
+        x = self.position.x
+        y = self.position.y
+        n = len(map)
+        m = len(map[0])
+        if x - 1 >= 0 and map[x - 1][y] != 1:
+            moves.append(position(x - 1, y))
+        if x + 1 < n and map[x + 1][y] != 1:
+            moves.append(position(x + 1, y))
+        if y - 1 >= 0 and map[x][y - 1] != 1:
+            moves.append(position(x, y - 1))
+        if y + 1 < m and map[x][y + 1] != 1:
+            moves.append((position(x, y + 1)))
+        return moves
 
 
 class Hider(Agent):
     def __init__(self, x: int, y: int) -> None:
         super().__init__(x, y)
         self.isFound = False
-    def move(self, moves : list[position]) -> position:
-        self.position = random.choice(moves)
+    def move(self, map : list[list[int]]) -> position:
+        self.position = random.choice(self._get_posible_moves(map))
+        return self.position
+    def annoucePos(self) -> position:
         return self.position
 
 class Seeker(Agent):
     def __init__(self, x: int, y: int) -> None:
         super().__init__(x, y)
         self.hiderLastPos = position(0, 0)
-    def move(self, moves : list[position]) -> position:
-        self.position = random.choice(moves)
+    def move(self, map : list[list[int]]) -> position:
+        self.position = random.choice(self._get_posible_moves(map))
         return self.position
 
 class GameMaster:
     def __init__(self, filename: str) -> None:
         self._hider = Hider(0, 0)
         self._seeker = Seeker(0, 0)
+        self._turn = 0
+        self._seekerAnnounceInterval = 2
         with open(filename, 'r') as file:
             self._n, self._m = [int(x) for x in next(file).split()]
             self._map = [[int(x) for x in line[:-1]] for line in file]
@@ -45,23 +67,13 @@ class GameMaster:
                         self._hider = Hider(i, j)
                     elif self._map[i][j] == 3:
                         self._seeker = Seeker(i, j)
+        self._hidden_map = copy.deepcopy(self._map)
+        self._hidden_map[self._hider.position.x][self._hider.position.y] = 0
+        self._hidden_map[self._seeker.position.x][self._seeker.position.y] = 0
+
 
     def is_game_over(self) -> bool:
         return self._hider.isFound
-
-    def _get_posible_moves(self, ag: Agent) -> list[position]:
-        moves : list[position] = []
-        x = ag.position.x
-        y = ag.position.y
-        if x - 1 >= 0 and self._map[x - 1][y] != 1:
-            moves.append(position(x - 1, y))
-        if x + 1 < self._n and self._map[x + 1][y] != 1:
-            moves.append(position(x + 1, y))
-        if y - 1 >= 0 and self._map[x][y - 1] != 1:
-            moves.append(position(x, y - 1))
-        if y + 1 < self._m and self._map[x][y + 1] != 1:
-            moves.append((position(x, y + 1)))
-        return moves
 
     def _update_screen(self, screen: pygame.Surface):
         screen.fill((255, 255, 255))
@@ -70,7 +82,7 @@ class GameMaster:
                 if self._map[i][j] == 1:
                     pygame.draw.rect(screen, (0, 0, 0), (j * 50, i * 50, 50, 50))
                 elif self._map[i][j] == 2:
-                    pygame.draw.rect(screen, (0, 255, 0), (j * 50, i * 50, 50, 50))
+                    pygame.draw.rect(screen, (0, 255, 100), (j * 50, i * 50, 50, 50))
                 elif self._map[i][j] == 3:
                     pygame.draw.rect(screen, (255, 0, 0), (j * 50, i * 50, 50, 50))
         pygame.display.flip()
@@ -87,13 +99,16 @@ class GameMaster:
                 self._update_screen(screen)
                 if self.is_game_over():
                     break
+                self._turn += 1
+                if self._turn % self._seekerAnnounceInterval == 0:
+                    self._seeker.hiderLastPos = self._hider.annoucePos()
                 previousSeekerPos = self._seeker.position
-                self._seeker.move(self._get_posible_moves(self._seeker))
+                self._seeker.move(self._hidden_map)
                 self._map[previousSeekerPos.x][previousSeekerPos.y] = 0
                 self._map[self._seeker.position.x][self._seeker.position.y] = 3
                 if self._seeker.position.x == self._hider.position.x and self._seeker.position.y == self._hider.position.y:
                     self._hider.isFound = True
-                pygame.time.delay(500)
+                pygame.time.Clock().tick(20)
 
 if __name__ == '__main__':
     game = GameMaster('map.txt')
